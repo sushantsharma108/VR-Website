89c15a89e6c01d6a593e3272ea110b33d327a461
Sushant Sharma <ssushants786@gmail.com>
2024-07-22 16:08:04 +0530

    setup the color-scheme:dark in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import logo from '../assets/logo.png'
+const Navbar = () => {
+    return (
+        <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
+            <div className="container px-4 mx-auto relative text-sm">
+                <div className="flex justify-center items-center">
+                    <div className="flex items-center flex-shrink-0">
+                        <img src={logo} alt="" />
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
