741443f1ab509e16a880d7a49e72c2c952acf5d7
Sushant Sharma <ssushants786@gmail.com>
2024-07-22 16:42:12 +0530

    removed vite svg image

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
