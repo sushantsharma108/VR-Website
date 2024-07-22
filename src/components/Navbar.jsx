a08c76674b6808b8b5bbd00329dfa406f14854e6
Sushant Sharma <ssushants786@gmail.com>
2024-07-22 20:32:11 +0530

    changed the span title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
                 <div className="flex justify-between items-center">
                     <div className="flex items-center flex-shrink-0">
                         <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-                        <span className="text-xl tracking-tight">VirtualR</span>
+                        <span className="text-xl tracking-tight">ViSys</span>
                     </div>
                     <ul className="hidden lg:flex ml-14 space-x-12">
                         {navItems.map((item, index) => (
