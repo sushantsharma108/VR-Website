a33a2721a46b150a392183bb262a895dada39f92
Sushant Sharma <ssushants786@gmail.com>
2024-07-23 01:07:48 +0530

    completed except css in workflow.jsx

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import code from '../assets/code.jpg';
 import { checklistItems } from '../constants';
+import { CircleCheckIcon } from 'lucide-react';
 const Workflow = () => {
     return (
         <div className=''>
@@ -9,14 +10,21 @@ const Workflow = () => {
                 <h1 className='text-6xl text-center mt-20'>Accelarate your <span className="bg-gradient-to-r from-blue-500 to-blue-900 text-transparent bg-clip-text"> coding workflow.</span></h1>
             </div>
 
-            <div className='flex mb-20'>
+            <div className='lg:flex mb-20'>
                 <div className='lg:w-1/2 mt-20'>
                     <img src={code} alt="" />
                 </div>
 
-                {checklistItems.map((item, index))}
-                <div className='flex lg:w-1/2 mt-20'>
-                    Checklist
+                <div className='lg:w-1/2 mt-20'>
+                    {checklistItems.map((item, index) => (
+                        <div key={index} className='flex mt-16 mb-10'>
+                            <p className='mt-0.5 mr-5'>{<CircleCheckIcon color="#00ff00" />}</p>
+                            <div>
+                                <h3 className='text-white-900'>{item.title}</h3>
+                                <p className='w-4/5 text-neutral-400'>{item.description}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
@@ -24,3 +32,6 @@ const Workflow = () => {
 }
 
 export default Workflow
+
+
+
