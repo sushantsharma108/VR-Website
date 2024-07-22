98451ca2fcf941810ec0acd54f0f7a120ec077a5
Sushant Sharma <ssushants786@gmail.com>
2024-07-22 17:40:30 +0530

    nested the hero component in a div in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-20 px-6">
+        <Hero/>
+      </div>
     </>
   )
 }
