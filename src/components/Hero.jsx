31a409ca9db9e196e0d3161e35487e2a99aaec75
Sushant Sharma <ssushants786@gmail.com>
2024-07-22 19:03:41 +0530

    hero section completed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -37,7 +37,7 @@ const Hero = () => {
                     className="sm:flex flex-col h-full rounded-lg lg:w-1/2 mx-2 my-4"
                 >
                     <source src={video1} type="video/mp4" />
-                    Your browser does not support the video tag.
+                    Your browser does not support the video tag
                 </video>
                 <video
                     autoPlay
@@ -46,7 +46,7 @@ const Hero = () => {
                     className="sm:flex flex-col h-full rounded-lg lg:w-1/2 mx-2 my-4"
                 >
                     <source src={video2} type="video/mp4" />
-                    Your browser does not support the video tag.
+                    Your browser does not support the video tag
                 </video>
             </div>
         </>
