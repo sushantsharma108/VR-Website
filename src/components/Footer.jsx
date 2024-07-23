a3f6ee77638998f0ae16d8b9b4048164f787b1a1
Sushant Sharma <ssushants786@gmail.com>
2024-07-23 17:04:06 +0530

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,34 +5,44 @@ import { communityLinks } from '../constants'
 
 const Footer = () => {
     return (
-        <div className='flex flex-wrap mt-10'> 
-            <div className='mt-5 mr-20'>
-                <h3>Resources</h3>
-                {resourcesLinks.map((item, index) => (
-                    <div>
-                        <a href={item.href}>{item.text}</a>
-                    </div>
-                ))}
-            </div>
+        <footer className='mt-20 border-t py-10'>
+            <div className='grid grid-cols-2 lg:grid-cols-3 gap-4'>
+                <div className='mt-5 mr-20'>
+                    <h3 className='text-md font-semibold mb-4'>Resources</h3>
+                    <ul className='space-y-2'>
+                        {resourcesLinks.map((item, index) => (
+                            <li key={index}>
+                                <a className='text-neutral-400 hover:text-white' href={item.href}>{item.text}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-            <div className='mt-5 mr-20'>
-                <h3>Platform</h3>
-                {platformLinks.map((item, index) => (
-                    <div>
-                        <a href={item.href}>{item.text}</a>
-                    </div>
-                ))}
-            </div>
+                <div className='mt-5 mr-20'>
+                    <h3 className='text-md font-semibold mb-4'>Platform</h3>
+                    <ul className='space-y-2'>
+                        {platformLinks.map((item, index) => (
+                            <li key={index}>
+                                <a className='text-neutral-400 hover:text-white' href={item.href}>{item.text}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
 
-            <div className='mt-5'>
-                <h3>Resources</h3>
-                {resourcesLinks.map((item, index) => (
-                    <div>
-                        <a href={item.href}>{item.text}</a>
-                    </div>
-                ))}
+                <div className='mt-5 mr-20'>
+                    <h3 className='text-md font-semibold mb-4'>Community</h3>
+                    <ul className='space-y-2'>
+                        {communityLinks.map((item, index) => (
+                            <li key={index}>
+                                <a className='text-neutral-400 hover:text-white' href={item.href}>{item.text}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
             </div>
-        </div>
+            <p className='mt-10 text-center text-sm text-neutral-50'>&copy; 2024 ViSys. All rights reserved.</p>
+        </footer>
     )
 }
 
