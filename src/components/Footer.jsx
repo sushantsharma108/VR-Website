ca840b98a9aa4ce4be214abfb092e285f2dbbca9
Sushant Sharma <ssushants786@gmail.com>
2024-07-23 17:14:07 +0530

    project completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ import { communityLinks } from '../constants'
 const Footer = () => {
     return (
         <footer className='mt-20 border-t py-10'>
-            <div className='grid grid-cols-2 lg:grid-cols-3 gap-4'>
+            <div className='grid grid-cols-2 lg:grid-cols-3 gap-4 lg:place-items-center'>
                 <div className='mt-5 mr-20'>
                     <h3 className='text-md font-semibold mb-4'>Resources</h3>
                     <ul className='space-y-2'>
