06a53ba7aa6306fbc7245d84ac83dcf8da55414e
Sushant Sharma <ssushants786@gmail.com>
2024-07-23 17:26:55 +0530

    removed the name span from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,7 @@ const Navbar = () => {
             <div className="container px-4 mx-auto relative lg:text-sm">
                 <div className="flex justify-between items-center">
                     <div className="flex items-center flex-shrink-0">
-                        <img className="h-10 w-10 mr-2" src={logo} alt="Logo" />
-                        <span className="text-xl tracking-tight">ViSys</span>
+                        <img className="h-10 w-10 mr-2 rounded-full" src={logo} alt="Logo" />
                     </div>
                     <ul className="hidden lg:flex ml-14 space-x-12">
                         {navItems.map((item, index) => (
